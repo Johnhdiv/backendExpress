ad1557a062b48a276ff2056d24d518e1cbbb8058
johnhdiv <johnhoyos767@gmail.com>
2024-12-11 12:50:27 -0500

    actualizacion

---
+++ controllers/holaController.js
@@ -0,0 +1,4 @@
+exports.holaMundo = (req,res)=>{
+    console.log("hola desde el controlador")
+    res.send("hola desde el controlador")
+  }
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require("express")
+const express= require("express")
+const holaRoutes = require("./routes/holaRoutes")
+
 
 const app = express()
 
 const PORT = 3006
+app.set("port", PORT)
 
-app.set("port",PORT)
+app.use("/api/hola",holaRoutes)
 
-app.get("/",(req,res)=>{
-    res.send("hola mundo")
+app.get("/", (req,res)=>{
+ res.send("hola mundo")
 })
+
 app.listen(PORT,()=>{
-    console.log(`server running in port ${PORT}`)
+  console.log(`server runing in port${PORT}`)
 })
